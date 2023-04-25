12c367549468f544a896b1d71a12353ef771e6cd
seeit360 <greg.bays@gmail.com>
2023-04-25 07:41:06 -0700

    2 files modified

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -62,17 +62,17 @@
             <small class="step">Step 1)</small>
             <p>Go to "System Settings"</p>
             <p>Scroll to Internet > Internet Settings</p>
-            <img alt="Step 1" class="fit" src="/assets/switch-step1.png" />
+            <img alt="Step 1" class="fit" src="https://raw.githubusercontent.com/seeit360/mc-pfaos/main/public/assets/switch-step1.png" />
           </li>
           <li>
             <small class="step">Step 2)</small>
             <p>Select "Change Settings"</p>
-            <img alt="Step 2" class="fit" src="/assets/switch-step2.png" />
+            <img alt="Step 2" class="fit" src="https://raw.githubusercontent.com/seeit360/mc-pfaos/main/public/assets/switch-step2.png" />
           </li>
           <li>
             <small class="step">Step 3)</small>
             <p>Change "DNS Settings" to "Manual"</p>
-            <img alt="Step 3" class="fit" src="/assets/switch-step3.png" />
+            <img alt="Step 3" class="fit" src="https://raw.githubusercontent.com/seeit360/mc-pfaos/main/public/assets/switch-step3.png" />
           </li>
           <li>
             <small class="step">Step 4)</small>
@@ -81,14 +81,14 @@
 
             <p>Set the Secondary DNS to "8.8.8.8" <small>(Google DNS)</small></p>
 
-            <img alt="Step 4" class="fit" src="/assets/switch-step4.png" />
+            <img alt="Step 4" class="fit" src="https://raw.githubusercontent.com/seeit360/mc-pfaos/main/public/assets/switch-step4.png" />
 
             <p><small>Issues? Other free DNS listings can be found here: <a title="Bedrock Connect" target="_blank" rel="noopener noreferrer" href="https://github.com/Pugmatt/BedrockConnect">https://github.com/Pugmatt/BedrockConnect</a></small></p>
           </li>
           <li>
             <small class="step">Step 5)</small>
             <p>Click "Save"</p>
-            <img alt="Step 5" class="fit" src="/assets/switch-step5.png" />
+            <img alt="Step 5" class="fit" src="https://raw.githubusercontent.com/seeit360/mc-pfaos/main/public/assets/switch-step5.png" />
           </li>
           <li>
             <small class="step">Step 6)</small>
@@ -185,7 +185,7 @@
       </ul>
     </section>
     <div class="section container">
-      <img class="logo" alt="Logo" src="/assets/cropped-logo.png">
+      <img class="logo" alt="Logo" src="https://raw.githubusercontent.com/seeit360/mc-pfaos/main/public/assets/cropped_logo.png">
     </div>
 
   </div>
